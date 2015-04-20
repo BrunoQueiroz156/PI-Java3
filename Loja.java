6e42c3e091401c5a532c2152186c21247fb5caca
BrunoQueiroz156 <burnout156@hotmail.com>
2015-04-20 12:59:36 -0300

    Create Loja.java
    Classe Loja

---
+++ Loja.java
@@ -0,0 +1,11 @@
+package ProjetodeLoja;
+
+import java.util.Scanner;
+
+public class Loja {
+	Scanner input = new Scanner(System.in);
+	Carro peçascarro = new Carro();
+	String Endereço;
+	String nome;
+    
+}

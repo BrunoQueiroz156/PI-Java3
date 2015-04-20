edddd8b5b3da79caf3ede6c4f73fa2cde8a09105
BrunoQueiroz156 <burnout156@hotmail.com>
2015-04-20 12:59:57 -0300

    Create Motocicleta.java

---
+++ Motocicleta.java
@@ -0,0 +1,16 @@
+package ProjetodeLoja;
+
+import java.util.ArrayList;
+
+public class Motocicleta {
+	String Chassi;
+    String Montadora;
+    String Modelo;
+    String Tipo; 
+    String Cor;
+    int Cilindrada;
+    int CapacidadeTanque;
+    float Preço;
+    
+    ArrayList<Motocicleta> EstoqueMotos = new ArrayList<>();
+}

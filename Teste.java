2162777fdd83c3ed42a58842724b7f0415e82b2a
BrunoQueiroz156 <burnout156@hotmail.com>
2015-04-22 12:09:56 -0300

    Update Teste.java

--- Teste.java
+++ Teste.java
@@ -4,40 +4,39 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Teste {
-	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
 		int quantidade;
 		String resposta;
-		//Loja loja = new Loja();
-	    Carro carro = new Carro();
-	    ArrayList<Carro> EstoqueCarros = new ArrayList<>();
-	    //ArrayList<Motocicleta> EstoqueMotos = new ArrayList<>();
-	    System.out.println("Digite a quantidade de carros que você queira adicionar");
-	    quantidade = input.nextInt();
-	    for(int i = 0;i < quantidade;i++){
-	    Carro addcarro = carro.adicionarCarro( "" , "" , "" , "" , "" , 0 , 0 );//aqui eu chamei o método adicionarCarro e atribui os valores para a variavel do tipo carro para adicionar facilmente na ArrayList do tipo Carro
-	    EstoqueCarros.add(addcarro);
-	    }
-	    
-	    System.out.println(" Você quer listar o Carro? ");
-	    resposta = input.next();
-	    if((resposta == "Sim") || (resposta == "sim" )){
-	    	Carro listar = new Carro();
-	    	listar.listarEstoqueCarro( "" , "" , "" , "" , "" , 0 , 0 , EstoqueCarros );
-	    }
-	    
-	    System.out.println(" Você quer Pesquisar um Carro? ");
-	    resposta = input.next();
-	    if((resposta == " Sim ") || (resposta == "sim " )){
-	    	Carro.PesquisarCarro( "" , "" , "" , "" , "" , 0 , 0 , EstoqueCarros );
-	    }
-	    
-	    System.out.println("Você quer Buscar um Carro pelo Chassi?");
-	    resposta = input.next();
-	    if((resposta == " Sim ") || (resposta == "sim" )){
-	    	Carro.BuscarCarro( "" , EstoqueCarros );
-	   }
+            Scanner input = new Scanner(System.in);
+                Carro carro = new Carro();
+                ArrayList<Carro> EstoqueCarros = new ArrayList<>();
+                //ArrayList<Motocicleta> EstoqueMotos = new ArrayList<>();              
+                System.out.println("Digite a quantidade de carros que você queira adicionar");
+                quantidade = input.nextInt();
+                for(int i = 0;i < quantidade;i++){
+                Carro addcarro = carro.adicionarCarro( "" , "" , "" , "" , "" , 0 , 0 );//aqui eu chamei o método adicionarCarro e atribui os valores para a variavel do tipo carro para adicionar facilmente na ArrayList do tipo Carro
+                EstoqueCarros.add(addcarro);
+                }
+                
+                System.out.println("Você quer listar o Carro?");
+                resposta = input.next();
+                if(resposta.equals("sim")){
+                carro.listarEstoqueCarro( "" , "" , "" , "" , "" , 0 , 0 , EstoqueCarros ); //método para listar o estoque de carros que você adicionou
+                }
+                
+               
+                System.out.println("Você quer Pesquisar um Carro?");
+                resposta = input.next();
+                if(resposta.equals("sim")){
+                   carro.PesquisarCarro( "" , "" , "" , "" , "" , 0 , 0 , EstoqueCarros ); //método para pesquisar o carro
+                }
+                          
+                System.out.println("Você quer Buscar um Carro pelo Chassi?");
+                resposta = input.next();
+                if(resposta.equals("sim")){
+                   carro.BuscarCarro( "" , EstoqueCarros );
+                   input.close();//sempre tem que fechar o input no final do código
+                }
+            }
 	  }
-	}
-
 //}

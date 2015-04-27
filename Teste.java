e4678134f697e9ba36a4ea657406e09277990071
BrunoQueiroz156 <burnout156@hotmail.com>
2015-04-27 12:36:42 -0300

    Update Teste.java

--- Teste.java
+++ Teste.java
@@ -1,42 +1,77 @@
 package ProjetodeLoja;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Teste {
 	public static void main(String[] args) {
-		int quantidade;
-		String resposta;
-                Scanner input = new Scanner(System.in);
-                Carro carro = new Carro();
-                ArrayList<Carro> EstoqueCarros = new ArrayList<>();
-                //ArrayList<Motocicleta> EstoqueMotos = new ArrayList<>();              
+	  Scanner input = new Scanner(System.in);
+	       int i;
+		   int quantidade;
+		   String resposta;  
+		   
                 System.out.println("Digite a quantidade de carros que você queira adicionar");
                 quantidade = input.nextInt();
-                for(int i = 0;i < quantidade;i++){
-                Carro addcarro = carro.adicionarCarro( "" , "" , "" , "" , "" , 0 , 0 );//aqui eu chamei o método adicionarCarro e atribui os valores para a variavel do tipo carro para adicionar facilmente na ArrayList do tipo Carro
-                EstoqueCarros.add(addcarro);
+                for(i = 1;i <= quantidade;i++){
+                Carro addcarro = Carro.adicionarCarro( "" , "" , "" , "" , "" , 0 , 0 );//aqui eu chamei o método adicionarCarro e atribui os valores para a variavel do tipo carro para adicionar facilmente na ArrayList do tipo Carro
+                Loja.EstoqueCarros.add(addcarro);
                 }
                 
-                System.out.println("Você quer listar o Carro?");
+                if(Loja.EstoqueCarros.size() > 0){
+                System.out.println("Você quer listar o(s) Carro(s)?");
                 resposta = input.next();
                 if(resposta.equals("sim")){
-                carro.listarEstoqueCarro( "" , "" , "" , "" , "" , 0 , 0 , EstoqueCarros ); //método para listar o estoque de carros que você adicionou
+                Carro.listarEstoqueCarro(Loja.EstoqueCarros); //método para listar o estoque de carros que você adicionou
+                }
                 }
                 
-               
+                if(Loja.EstoqueCarros.size() > 0){
                 System.out.println("Você quer Pesquisar um Carro?");
                 resposta = input.next();
                 if(resposta.equals("sim")){
-                   carro.PesquisarCarro( "" , "" , "" , "" , "" , 0 , 0 , EstoqueCarros ); //método para pesquisar o carro
+                   Carro.PesquisarCarro( "" , "" , "" , "" , "" , 0 , 0 , Loja.EstoqueCarros ); //método para pesquisar o carro
                 }
-                          
+                }
+                
+                if(Loja.EstoqueCarros.size() > 0){
                 System.out.println("Você quer Buscar um Carro pelo Chassi?");
                 resposta = input.next();
                 if(resposta.equals("sim")){
-                   carro.BuscarCarro( "" , EstoqueCarros );
-                   input.close();//sempre tem que fechar o input no final do código
+                   Carro.BuscarCarro( "" , Loja.EstoqueCarros );         
                 }
+                }
+                
+                if(Loja.EstoqueMotos.size() > 0){
+                System.out.println("Digite a quantidade de motos que você queira adicionar");
+                quantidade = input.nextInt();
+                for(i = 0;i <= quantidade;i++){
+                	Motocicleta addmotos = Motocicleta.adicionarMotocicleta( "" , "" , "" , "" , "" , 0 , 0 , 0 );
+                	Loja.EstoqueMotos.add(addmotos);
+                }
+                }
+                
+                if(Loja.EstoqueCarros.size() > 0){
+                System.out.println("Você quer listar a(s) Moto(s)?");
+                resposta = input.next();                
+                if(resposta.equals("sim")){
+                	Motocicleta.listarMotocicleta( Loja.EstoqueMotos );
+                }
+                }
+                
+                if(Loja.EstoqueMotos.size() > 0){
+                	System.out.println("Você quer pesquisar uma Moto?");
+                	resposta = input.next();
+                	if(resposta.equals("sim")){
+                		Motocicleta.pesquisarMotocicleta( "" , "" , "" , "" , "" , 0 , 0 , 0 );
+                	}
+                }
+                
+                if(Loja.EstoqueMotos.size() > 0){
+                	System.out.println("Você quer buscar uma moto pelo chassi?");
+                	resposta = input.next();
+                	if(resposta.equals("sim")){
+                		Motocicleta.buscarMotocicleta( "" , Loja.EstoqueMotos );
+                	}
+                }
+                input.close();//sempre tem que fechar o input no final do código
             }
 	  }
-//}
